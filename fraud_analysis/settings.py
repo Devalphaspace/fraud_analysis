d26bc1a9c41d6f9bd1eae8dcfd44aaffe7b3c819
TanmoyTSSaha <tanmoytssaha@gmail.com>
2023-11-09 13:52:38 +0530

    added url

--- fraud_analysis/settings.py
+++ fraud_analysis/settings.py
@@ -134,6 +134,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:3000",
     "http://127.0.0.1:5173",
     "http://localhost:5173",
+    'https://assignment-blog-mu.vercel.app/',
 ]
 
 CORS_ALLOW_ALL_ORIGINS = True
