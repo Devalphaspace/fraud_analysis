b52d17a80980ff56b3bf44a929e2745fd35314e2
TanmoyTSSaha <tanmoytssaha@gmail.com>
2023-11-09 14:00:57 +0530

    cors issue

--- fraud_analysis/settings.py
+++ fraud_analysis/settings.py
@@ -130,6 +130,8 @@ CORS_ALLOWED_ORIGIN_REGEXES = [
 ]
 
 CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000/",
+    "http://127.0.0.1:3000/",
     "http://localhost:3000",
     "http://127.0.0.1:3000",
     "http://127.0.0.1:5173",
