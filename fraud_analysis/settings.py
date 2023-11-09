dd1f02ac9261d9560cbbb207bd9c28e1c062e33d
TanmoyTSSaha <tanmoytssaha@gmail.com>
2023-11-09 13:40:25 +0530

    cors issue

--- fraud_analysis/settings.py
+++ fraud_analysis/settings.py
@@ -120,19 +120,12 @@ USE_I18N = True
 
 USE_TZ = True
 
-CSRF_TRUSTED_ORIGINS = [
-    "http://127.0.0.1:5173",
-    "http://127.0.0.1:3000",
-    "http://localhost:5173",
-    "http://localhost:3000",
-]
-
-CORS_ALLOWED_ORIGINS = [
-    "http://127.0.0.1:5173",
-    "http://127.0.0.1:3000",
-    "http://localhost:5173",
-    "http://localhost:3000",
-]
+# CORS_ALLOWED_ORIGINS = [
+#     "http://localhost:3000",
+#     "http://127.0.0.1:3000",
+#     "http://127.0.0.1:5173",
+#     "http://localhost:5173",
+# ]
 
 CORS_ALLOW_ALL_ORIGINS = True
 
